f8d637d9db2d2e2586a798c1aeae8dcb8a950b94
emilstricker <26085460+emilstricker@users.noreply.github.com>
2024-01-10 21:46:43 +0100

    Google popup

---
+++ public/config.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    // Your web app's Firebase configuration here
+    // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+    apiKey: "AIzaSyD942kWP9Ei1ZCntA1lqPqXukpX_LMIVeo",
+    authDomain: "grams-953c8.firebaseapp.com",
+    databaseURL: "https://grams-953c8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "grams-953c8",
+    storageBucket: "grams-953c8.appspot.com",
+    messagingSenderId: "292028283965",
+    appId: "1:292028283965:web:44ed869a7c07c873a4be54",
+    measurementId: "G-E6D1KYFV22"
+  };
