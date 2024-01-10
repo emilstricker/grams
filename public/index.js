add5e9189603b2672e47e0add34d80254e78d8e4
emilstricker <26085460+emilstricker@users.noreply.github.com>
2024-01-10 22:34:19 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
